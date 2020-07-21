4d92320e58bce2c737b556289a431756208ad8fe
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-07-21 14:40:02 -0400

    add routes and a fetch method to test out communication of backend with frotend

--- book-quotes-frontend/src/index.js
+++ book-quotes-frontend/src/index.js
@@ -1,2 +1,7 @@
 console.log("testing...")
 const pageHeading = document.getElementById("heading")
+
+const BACKEND_URL = 'https://localhost:3000';
+fetch(`${BACKEND_URL}/test`)
+  .then(response => response.json())
+  .then(parsedResponse => console.log(parsedResponse));
