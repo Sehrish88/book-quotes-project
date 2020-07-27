9243c671b72aaf68a75962a68b868cc367c5bd56
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-07-27 12:07:21 -0400

    fix routes.rb file to get rid of duplicate code

--- book-quotes-backend/config/routes.rb
+++ book-quotes-backend/config/routes.rb
@@ -1,6 +1,6 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  Rails.application.routes.draw do
+ Rails.application.routes.draw do
+  resources :quotes
+  resources :books
     get '/test', to: 'application#test'
   end
-end
+
