a3c2df590efcc3b42ec760af3c13effd55a0fbeb
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-07-27 17:30:27 -0400

    create a frontend book object

---
+++ book-quotes-frontend/src/book.js
@@ -0,0 +1,8 @@
+class Book{
+    constructor(title, author, genre){
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+    }
+    //instance method that will render object to DOM
+}
