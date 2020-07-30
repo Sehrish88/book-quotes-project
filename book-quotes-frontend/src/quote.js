26f05bdb7951520c80a91c598920e8cc35bedb51
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-07-30 18:21:21 -0400

    no changes

--- book-quotes-frontend/src/quote.js
+++ book-quotes-frontend/src/quote.js
@@ -14,7 +14,7 @@ class Quote{
        const addedquotes =  
         `
         <ul>
-        <li id="quote"> ${this.id} - <i><b>${this.content}</i></b> - (<b>${this.book.title}</b>, ${this.book.author}, ${this.book.genre}) <button class="delete-bttn" data-id=${this.id} onclick="deleteQuote()" >  Delete </button></li>  
+        <li id="quote"><i><b>${this.content}</i></b> - (<b>${this.book.title}</b>, ${this.book.author}, ${this.book.genre}) <button class="delete-bttn" data-id=${this.id} onclick="deleteQuote()" >  Delete </button></li>  
         </ul> 
         `
         quotesDiv.innerHTML +=  addedquotes
